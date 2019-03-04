0edb6aaa4148d80d1a1a0415ba52aaff82fd6108
Steven <stevenbdf@gmail.com>
2019-03-04 16:09:48 -0600

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/evaluators_system/precache-manifest.f8221be5d498b465887513b193b16511.js"
+  "/evaluators-deploy/precache-manifest.09f446706666b69242679440de234a52.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/evaluators_system/index.html", {
+workbox.routing.registerNavigationRoute("/evaluators-deploy/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
